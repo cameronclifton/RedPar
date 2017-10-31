b15878ae8cd35ed01dae810ada9d45994b756f3a
Adam Svetec <adamsvetec@gmail.com>
2017-10-30 20:33:00 -0400

    add sample handler registration and callback

--- src/reint.cpp
+++ src/reint.cpp
@@ -1,8 +1,18 @@
 #include "reint.h"
 
+int TestCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+    RedisModule_ReplyWithLongLong(ctx,1);
+    return REDISMODULE_OK;
+}
+
 extern "C" int RedisModule_OnLoad(RedisModuleCtx *ctx) {
     // Register the module itself – it’s called example and has an API version of 1
     if (RedisModule_Init(ctx, "example", 1, REDISMODULE_APIVER_1) == REDISMODULE_ERR) {
+        return REDISMODULE_ERR;
+    }
+
+    // register our command – it is a write command, with one key at argv[1]
+    if (RedisModule_CreateCommand(ctx, "RETEST", TestCommand, "readonly", 1, 1, 1) == REDISMODULE_ERR) {
         return REDISMODULE_ERR;
     }
 
